7a04e907cc7a4bbc0dd17e85881b29007b28c81f
uknow-home <yoon6763@naver.com>
2023-05-05 00:17:53 +0900

    PostService

--- src/main/java/aws/springboot/webservice/domain/posts/PostsRepository.java
+++ src/main/java/aws/springboot/webservice/domain/posts/PostsRepository.java
@@ -3,5 +3,4 @@ package aws.springboot.webservice.domain.posts;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PostsRepository extends JpaRepository<Posts, Long> {
-
 }
