e9b479580043d25d6aeaf7df576e84200fdd92f8
uknow-mac <yoon6763@naver.com>
2023-05-10 21:39:29 +0900

    Auditing

--- src/main/java/aws/springboot/webservice/WebserviceApplication.java
+++ src/main/java/aws/springboot/webservice/WebserviceApplication.java
@@ -2,12 +2,14 @@ package aws.springboot.webservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing // JPA Auditing 활성화
 @SpringBootApplication
 public class WebserviceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(WebserviceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(WebserviceApplication.class, args);
+    }
 
 }
